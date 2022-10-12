712eebbc1537ab489b96202ccc3e4f354d659c82
gerardmallol <gerardmallol01@gmail.com>
2022-10-12 18:01:07 +0200

    moving around errors

--- navigation.js
+++ navigation.js
@@ -5,11 +5,13 @@ const previousSlide = document.getElementById("previousSlide");
 const nextChapter = document.getElementById("nextChapter");
 const previousChapter = document.getElementById("previousChapter");
 const restart = document.getElementById("restart");
+
 const presentation = document.getElementById("presentation");
 const chapters = document.getElementsByClassName("chapter");
 const chapterArr = Array.from(chapters);
-const chapterLengths = chapterArr.map((chapter) => parseInt(chapter.style.width.slice(0, -4)));
-const slides = chapterArr.map((chapter) => 0);
+
+const chapterLengths = chapterArr.map((chapter) => parseInt(chapter.style.width.slice(0, -4)));         // Array which saves how many slides are for each chapter
+const slides = chapterArr.map((chapter) => 0);                                                          // Array which saves the position in which we are
 const titles = document.querySelectorAll("#titles > span");
 
 
@@ -18,6 +20,7 @@ function moveChapter(amount) {
     chapter += amount;
     if (chapter <= 0) {
         previousChapter.disabled = true;
+        nextChapter.disabled = false;
         chapter = 0;
     } else if (chapter >= chapterLengths.length - 1) {
         nextChapter.disabled = true;
@@ -26,6 +29,12 @@ function moveChapter(amount) {
         nextChapter.disabled = false;
         previousChapter.disabled = false;
     }
+
+    if(slides[chapter] <= 0) {
+        previousSlide.disabled = true;
+        nextSlide.disabled = false;
+    }
+    
     titles[chapter].querySelector(".position").innerHTML = slides[chapter] + 1;
     titles[chapter].style.display = "inline";
     presentation.style.translate = "-" + slides[chapter] + "00vw -" + chapter + "00vh";
@@ -51,7 +60,7 @@ function moveSlide(amount) {
 }
 
 function fadeButtons() {
-    buttons.style.opacity = 0.05;
+    buttons.style.opacity = 0.25;
 }
 
 function unfadeButtons(e) {
@@ -63,12 +72,17 @@ function unfadeButtons(e) {
 
 nextSlide.addEventListener("click", (e) => { moveSlide(1) });
 previousSlide.addEventListener("click", (e) => { moveSlide(-1) });
+
 nextChapter.addEventListener("click", (e) => { moveChapter(1);  });
 previousChapter.addEventListener("click", (e) => { moveChapter(-1); });
+
 restart.addEventListener("click", (e) => { moveChapter(- chapter); moveSlide(-slides[chapter]) });
+
 buttons.addEventListener("mouseenter", unfadeButtons);
 buttons.addEventListener("mouseleave", fadeButtons);
 
 titles[chapter].style.display = "inline";
+
 const t = setTimeout(fadeButtons, 2500);
+
 moveSlide(0);
